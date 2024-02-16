5d0019230ed5005e788aa03c6a2b0564d8bf5651
kadchenko <dmitry.kadchenko@gmail.com>
2024-02-16 16:18:08 +0200

    Change logo and favicon

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import Logo from '@assets/icons/logo.svg';
 
 const Header = () => {
   return (
-    <div className="cp flex items-center py-2">
+    <div className="cp flex items-center gap-2 py-2">
       <Logo /> Rando
     </div>
   );
